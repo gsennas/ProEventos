91f5066f26bdf7d21d4c5ac6621a0738af1ac7a0
Glaucio Senna <gsennaeua@gmail.com>
2022-12-04 14:22:35 -0300

    Introducao ao EFCore

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Model;
 
 namespace ProEventos.API.Controllers
@@ -11,60 +12,31 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        
-        List<Evento> _listaEventos = new List<Evento>(){
-                new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11",
-                Local = "Belo Horizonte",
-                QuantidadePessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                Lote = "1",
-                ImagemUrl = "teste1.jpg"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "CSS",
-                Local = "Rio de Janeiro",
-                QuantidadePessoas = 250,
-                DataEvento = DateTime.Now.ToString(),
-                Lote = "2",
-                ImagemUrl = "teste2.jpg"}
-                };
+        private readonly DataContext _context;
 
-        public EventosController()
+        public EventosController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         { 
-            return _listaEventos; 
+            return _context.Eventos.ToList(); 
         }
 
         [HttpGet("{id}")]
         public Evento Get(int id)
         { 
-            return _listaEventos.Where(c=> c.EventoId.Equals(id)).FirstOrDefault(); 
+            return  _context.Eventos.FirstOrDefault(c=> c.EventoId.Equals(id)); 
         }
 
          [HttpPost]
-        public string Post()
+        public Evento Post(Evento evento)
         {
-            _listaEventos.Add(
-            new Evento(){
-                EventoId = 3,
-                Tema = "CSS",
-                Local = "Rio de Janeiro",
-                QuantidadePessoas = 250,
-                DataEvento = DateTime.Now.ToString(),
-                Lote = "2",
-                ImagemUrl = "teste2.jpg"
-                });
-                if(_listaEventos.Where(c=> c.EventoId.Equals(3)).Any())
-                return "Sucesso";
+            
 
-                return "Erro";
+                return evento;
         }
 
         [HttpPut]
